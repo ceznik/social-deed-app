b5d06bc275e2af24a639222a611ef1e7a89a5d23
ceznik <ceznik@verizon.net>
2016-05-17 00:42:26 -0400

    deeds.html plus api-routes for deeds

---
+++ app/config/connection.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Shawn on 5/16/2016.
+ */
+var Sequelize = require("sequelize");
+
+
+var source = {
+    jawsDB: {
+        port: 3306,
+        host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'iss7a2c2lcow0icm',
+        password: "sylgrzdf9u529sia",
+        database: "zbilm8sq8aqxtp9j"
+
+    }
+};
+
+var selectedSource = source.jawsDB;
+
+var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
+    host: selectedSource.host,
+    dialect: 'mysql',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+
+});
+console.log("sequelize connection initiated.");
+module.exports = sequelize;
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -9,7 +9,7 @@ module.exports=function(app){
 	});
 	app.get('/profile',function(req,res){
 		res.sendFile(path.join(__dirname+ '/../public/profile.html'))
-	})
+	});
 	app.use(function(req,res){
 		res.sendFile(path.join(__dirname+'/../public/index.html'))
 	});
